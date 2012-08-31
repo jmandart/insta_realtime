bf5a087ae19c82cdf240676c23b2401e177e2084
Julien <julien@piratalondon.com>
2012-08-31 10:40:06 +0100

    push

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var express = require('express'),
 	instagram = require('instagram-node-lib');
 
 //Set Instagram
-instagram.set('client_id', 'a1cf6651ba4743498540dbd8c4027f9e');
-instagram.set('client_secret', '266f4960d8234179985a4554798e2ea1');
-instagram.set('callback_url', 'http://staging1.pirata.co.uk:3001/callback');
-instagram.set('redirect_uri', 'http://staging1.pirata.co.uk:3001/index');
+// instagram.set('client_id', 'a1cf6651ba4743498540dbd8c4027f9e');
+// instagram.set('client_secret', '266f4960d8234179985a4554798e2ea1');
+// instagram.set('callback_url', 'http://staging1.pirata.co.uk:3001/callback');
+// instagram.set('redirect_uri', 'http://staging1.pirata.co.uk:3001/index');
 
 
 // instagram.tags.info({
@@ -31,15 +31,15 @@ app.get('/index', function(req, res){
 	});
 });
 
-app.get('/callback', function(req, res){
+// app.get('/callback', function(req, res){
 
-	instagram.subscriptions.handshake(req, res);
+// 	instagram.subscriptions.handshake(req, res);
+	
+// 	// res.render('callback', {
+// 	// 	title: 'REAL TIME - CALLBACK'
+// 	// });
+// });
 
-	// res.render('callback', {
-	// 	title: 'REAL TIME - CALLBACK'
-	// });
-});
-
-instagram.subscriptions.subscribe({ object: 'tag', object_id: 'jayistesting' });
+// instagram.subscriptions.subscribe({ object: 'tag', object_id: 'jayistesting' });
 
 app.listen(3001);
