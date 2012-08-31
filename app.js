ca6eb4e5fe36964db7a6f0656cb166fab161ad82
Julien <julien@piratalondon.com>
2012-08-31 16:39:03 +0100

    push

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express'),
 	app = express(),
+	server = require('http').createServer(app),
 	engines = require('consolidate'),
 	Instagram = require('instagram-node-lib'),
-	qs = require('querystring');
+	qs = require('querystring'),
+	io = require('socket.io').listen(app);
 
 //Set Instagram
 Instagram.set('client_id', 'a1cf6651ba4743498540dbd8c4027f9e');
@@ -21,7 +23,6 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use("/css", express.static(__dirname + '/css'));
 
-//app.set('views options', {layout: 'layout.html'});
 
 app.get('/index', function(req, res){
 
@@ -29,7 +30,6 @@ app.get('/index', function(req, res){
 		complete: function(data, pagination){
 	      // data is a javascript object/array/null matching that shipped Instagram
 	      // when available (mostly /recent), pagination is a javascript object with the pagination information
-
 
 	      console.log('pagination', pagination);
 	      // console.log('______________________________________________');
@@ -41,6 +41,7 @@ app.get('/index', function(req, res){
 			title: 'REAL TIME',
 			data: data
 		});
+
 	    },
 	  	error: function(errorMessage, errorObject, caller){
 	      // errorMessage is the raised error message
@@ -73,54 +74,11 @@ app.post('/callback', function(req, res){
         var POST = qs.parse(body);
         // use POST
         console.log('POST', POST);
+        res.send({more: 'BOOM'});
 
     });
 
     // return null;
 });
 
-// app.get('/index', function(request, response){
-
-// var url = Instagram.oauth.authorization_url({
-//   scope: 'comments likes' // use a space when specifying a scope; it will be encoded into a plus
-// });
-
-
-
-// var token = Instagram.oauth.ask_for_access_token({
-//     request: request,
-//     response: response,
-//     url: url,
-//     redirect: 'http://staging1.pirata.co.uk:3001/index', // optional
-//     complete: function(params, response){
-
-//     console.log('C: params', params);
-//     console.log('C: response', response);
-
-//       // params['access_token']
-//       // params['user']
-
-//       response.writeHead(200, {'Content-Type': 'text/plain'});
-//       // or some other response ended with
-//       response.end();
-//     },
-//     error: function(errorMessage, errorObject, caller, response){
-//     	console.log('E: response', response);
-//       // errorMessage is the raised error message
-//       // errorObject is either the object that caused the issue, or the nearest neighbor
-//       // caller is the method in which the error occurred
-//       response.writeHead(406, {'Content-Type': 'text/plain'});
-//       // or some other response ended with
-//       response.end();
-//     }
-//   });
-
-// console.log('token', token);
-
-// response.render('index', {
-// 		title: 'REAL TIME'
-// 	});
-//   // return null;
-// });
-
-app.listen(3001);
+server.listen(3001);
