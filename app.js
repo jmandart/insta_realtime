883f70278f78d403a3f1b2c2551e0288bfb5b0da
Julien <julien@piratalondon.com>
2012-09-05 15:06:26 +0100

    push

--- app.js
+++ app.js
@@ -72,37 +72,39 @@ console.log('BOOM0');
 		//var body = '';
 
 	    request.on('data', function (data) {
+
+	    	console.log(data.body);
 	        //body += data;
-	        data.body.forEach(function(notificationOjb){
-				    // Every notification object contains the id of the geography
-				    // that has been updated, and the photo can be obtained from
-				    // that geography
-				    https.get({
-				      host: 'api.instagram.com',
-				      path: '/v1/tags/' + notificationOjb.object_id + '/media/recent' +
-				      '?' + qs.stringify({client_id: process.env.instagram_client_id,count: 1}),
-				    }, function(res){
-				      var raw = "";
+	     //    data.body.forEach(function(notificationOjb){
+				  //   // Every notification object contains the id of the geography
+				  //   // that has been updated, and the photo can be obtained from
+				  //   // that geography
+				  //   https.get({
+				  //     host: 'api.instagram.com',
+				  //     path: '/v1/tags/' + notificationOjb.object_id + '/media/recent' +
+				  //     '?' + qs.stringify({client_id: process.env.instagram_client_id,count: 1}),
+				  //   }, function(res){
+				  //     var raw = "";
 
-				      res.on('data', function(chunk) {
-				        raw += chunk;
-				      });
+				  //     res.on('data', function(chunk) {
+				  //       raw += chunk;
+				  //     });
 
 
-				      // When the whole body has arrived, it has to be a valid JSON, with data,
-				      // and the first photo of the date must to have a location attribute.
-				      // If so, the photo is emitted through the websocket
-				      res.on('end', function() {
-				        var response = JSON.parse(raw);
-				        if(response['data'].length > 0 && response['data'][0]['location'] != null) {
-				          io.sockets.emit('photo', raw);
-				        } else {
-				          console.log("ERROR: %s", util.inspect(response['meta']));
-				        }
-				      });
+				  //     // When the whole body has arrived, it has to be a valid JSON, with data,
+				  //     // and the first photo of the date must to have a location attribute.
+				  //     // If so, the photo is emitted through the websocket
+				  //     res.on('end', function() {
+				  //       var response = JSON.parse(raw);
+				  //       if(response['data'].length > 0 && response['data'][0]['location'] != null) {
+				  //         io.sockets.emit('photo', raw);
+				  //       } else {
+				  //         console.log("ERROR: %s", util.inspect(response['meta']));
+				  //       }
+				  //     });
 
-				    });
-				  });
+				  //   });
+				  // });
 	    });
 
 	    // request.on('end', function () {
