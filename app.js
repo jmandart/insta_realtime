11a80197937f05ba717d9f2fae086a79eadf371d
Julien <julien@piratalondon.com>
2012-09-05 12:17:01 +0100

    push

--- app.js
+++ app.js
@@ -86,49 +86,49 @@ app.post('/callback', function(req, res){
 });
 
 
-function getNewImages(){
+// function getNewImages(){
 
-	console.log('BOOM2');
-	//console.log('indexImage', indexImage);
+// 	console.log('BOOM2');
+// 	//console.log('indexImage', indexImage);
 
-	Instagram.tags.recent({ name: 'jayistesting',
-		complete: function(data, pagination){
-	      // data is a javascript object/array/null matching that shipped Instagram
-	      // when available (mostly /recent), pagination is a javascript object with the pagination information
+// 	Instagram.tags.recent({ name: 'jayistesting',
+// 		complete: function(data, pagination){
+// 	      // data is a javascript object/array/null matching that shipped Instagram
+// 	      // when available (mostly /recent), pagination is a javascript object with the pagination information
 
-	    //sendNewImage(data[0]);
-	    //io.sockets.emit('add_image', { data: image });
-	    //console.log(data[0]);
+// 	    //sendNewImage(data[0]);
+// 	    //io.sockets.emit('add_image', { data: image });
+// 	    //console.log(data[0]);
 
-	    newImages.push(data[0]);
-	    console.log(newImages.length);
-	    //sendNewImages();
+// 	    newImages.push(data[0]);
+// 	    console.log(newImages.length);
+// 	    //sendNewImages();
 
-	    },
-	  	error: function(errorMessage, errorObject, caller){
-	      // errorMessage is the raised error message
-	      // errorObject is either the object that caused the issue, or the nearest neighbor
-	      // caller is the method in which the error occurred
-	    } 
+// 	    },
+// 	  	error: function(errorMessage, errorObject, caller){
+// 	      // errorMessage is the raised error message
+// 	      // errorObject is either the object that caused the issue, or the nearest neighbor
+// 	      // caller is the method in which the error occurred
+// 	    } 
 		
-	});
-}
+// 	});
+// }
 
-function sendNewImages(){
+// function sendNewImages(){
 	
-	// // console.log('timer is on --------');
-	//  console.log('newImages.length1', newImages.length);
-	// // console.log('--------');
-	// if(newImages.length > 0){
-	// 	console.log('newImages.length2', newImages.length);
-	// 	console.log('sendNewImages', newImages[0]);
-	// 	//io.sockets.emit('add_image', { data: '' });
-	// 	newImages.length = 0;
-	// 	// _.forEach(newImages, function(image){
-	// 	// 	io.sockets.emit('add_image', { data: image });
-	// 	// });
-	// }
-}
+// 	// // console.log('timer is on --------');
+// 	//  console.log('newImages.length1', newImages.length);
+// 	// // console.log('--------');
+// 	// if(newImages.length > 0){
+// 	// 	console.log('newImages.length2', newImages.length);
+// 	// 	console.log('sendNewImages', newImages[0]);
+// 	// 	//io.sockets.emit('add_image', { data: '' });
+// 	// 	newImages.length = 0;
+// 	// 	// _.forEach(newImages, function(image){
+// 	// 	// 	io.sockets.emit('add_image', { data: image });
+// 	// 	// });
+// 	// }
+// }
 
 //setInterval(sendNewImages,3000);
 
