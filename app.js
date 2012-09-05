fbc31f072a1ab5ce1cbbb134edbe9d7c883866a7
Julien <julien@piratalondon.com>
2012-09-05 16:47:47 +0100

    push

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ function getNewImages(){
 	    // when available (mostly /recent), pagination is a javascript object with the pagination information
 
 	    	//sendNewImage(data[0]);
-	   		//io.sockets.emit('photo', { img_url: data[0].images.low_resolution.url });
+	   		io.sockets.emit('photo', { img_url: data[0].images.low_resolution.url, full_name: data[0].user.full_name, likes: data[0].likes.count });
 	    	//console.log(data[0]);
 
 	    	console.log('data[0].images.low_resolution.url', data[0].images.low_resolution.url);
