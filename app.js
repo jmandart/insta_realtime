2fba1a02ba9d713b2a76ae55d6d656a3a13a5b2e
Julien <julien@piratalondon.com>
2012-09-05 15:40:04 +0100

    push

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var express = require('express'),
 	Instagram = require('instagram-node-lib'),
 	qs = require('querystring'),
 	async = require('async'),
-	util = require('util'),
 	_ = require('underscore');
 
 //Set Instagram
@@ -30,7 +29,7 @@ app.use("/css", express.static(__dirname + '/css'));
 var newImages = [];
 
 
-app.get('/', function(req, res){
+app.get('/index', function(req, res){
 
 	var images = Instagram.tags.recent({ name: 'jayistesting',
 		complete: function(data, pagination){
@@ -53,134 +52,67 @@ app.get('/', function(req, res){
 
 app.get('/callback', function(req, res){
 
-	// var handshake =  Instagram.subscriptions.handshake(req, res);
-	// console.log('handshake', handshake);
-	if(req.param("hub.challenge") != null){
-    	res.send(req.param("hub.challenge"));
-	} else {
-	    console.log("ERROR on suscription request: %s", util.inspect(req));
-	}
+	var handshake =  Instagram.subscriptions.handshake(req, res);
+	console.log('handshake', handshake);
 });
 
 
-app.post('/callback', function(request, response){
-	//
-
-console.log('BOOM0');
+app.post('/callback', function(req, res){
+	console.log('BOOM0');
 	//setTimeout(function(){
-		//console.log('BOOM1');
+		console.log('BOOM1');
 		var body = '';
 
-		console.log(request.get('body'));
-
-	    request.on('data', function (data) {
-
-	    	//console.log(data);
+	    req.on('data', function (data) {
 	        body += data;
-	     //    data.body.forEach(function(notificationOjb){
-				  //   // Every notification object contains the id of the geography
-				  //   // that has been updated, and the photo can be obtained from
-				  //   // that geography
-				  //   https.get({
-				  //     host: 'api.instagram.com',
-				  //     path: '/v1/tags/' + notificationOjb.object_id + '/media/recent' +
-				  //     '?' + qs.stringify({client_id: process.env.instagram_client_id,count: 1}),
-				  //   }, function(res){
-				  //     var raw = "";
-
-				  //     res.on('data', function(chunk) {
-				  //       raw += chunk;
-				  //     });
-
-
-				  //     // When the whole body has arrived, it has to be a valid JSON, with data,
-				  //     // and the first photo of the date must to have a location attribute.
-				  //     // If so, the photo is emitted through the websocket
-				  //     res.on('end', function() {
-				  //       var response = JSON.parse(raw);
-				  //       if(response['data'].length > 0 && response['data'][0]['location'] != null) {
-				  //         io.sockets.emit('photo', raw);
-				  //       } else {
-				  //         console.log("ERROR: %s", util.inspect(response['meta']));
-				  //       }
-				  //     });
-
-				  //   });
-				  // });
 	    });
 
-	    request.on('end', function () {
+	    req.on('end', function () {
 
 	        var POST = qs.parse(body);
 	        // use POST
 	        console.log('POST', POST);
-
-
-	        	    https.get({
-				      host: 'api.instagram.com',
-				      path: '/v1/tags/' + POST.object_id + '/media/recent' +
-				      '?' + qs.stringify('a1cf6651ba4743498540dbd8c4027f9e'),
-				    }, function(res){
-				      var raw = "";
-
-				      res.on('data', function(chunk) {
-				        raw += chunk;
-				      });
-
-
-				      // When the whole body has arrived, it has to be a valid JSON, with data,
-				      // and the first photo of the date must to have a location attribute.
-				      // If so, the photo is emitted through the websocket
-				      res.on('end', function() {
-				        var response = JSON.parse(raw);
-				        if(response['data'].length > 0 && response['data'][0]['location'] != null) {
-				          io.sockets.emit('photo', raw);
-				        } else {
-				          console.log("ERROR: %s", util.inspect(response['meta']));
-				        }
-				      });
-
-				    });
-	        
+	        getNewImages();
 	    });
 
-	    // request.on('close', function () {
-	    // 	//getNewImages();
+	    req.on('close', function () {
+	    	//getNewImages();
 	    	
-	    // });
-
-  response.writeHead(200);
+	    });
+	    res.writeHead(200);
+	//}, 2000);
+	
     
 });
 
 
-// function getNewImages(){
+function getNewImages(){
 
-// 	console.log('BOOM2');
-// 	//console.log('indexImage', indexImage);
+	console.log('BOOM2');
+	//console.log('indexImage', indexImage);
 
-// 	Instagram.tags.recent({ name: 'jayistesting',
-// 		complete: function(data, pagination){
-// 	      // data is a javascript object/array/null matching that shipped Instagram
-// 	      // when available (mostly /recent), pagination is a javascript object with the pagination information
+	Instagram.tags.recent({ name: 'jayistesting',
+		complete: function(data, pagination){
+	      // data is a javascript object/array/null matching that shipped Instagram
+	      // when available (mostly /recent), pagination is a javascript object with the pagination information
 
-// 	    //sendNewImage(data[0]);
-// 	   //io.sockets.volatile.emit('add_image', { data: 'BOOM' });
-// 	    console.log(data[0]);
+	    //sendNewImage(data[0]);
+	   //io.sockets.emit('add_image', { data: 'BOOM' });
+	    console.log(data[0]);
 
-// 	   // newImages.push(data[0]);
-// 	    //console.log(newImages.length);
-// 	    //sendNewImages();
+	   // newImages.push(data[0]);
+	    //console.log(newImages.length);
+	    //sendNewImages();
 
-// 	    },
-// 	  	error: function(errorMessage, errorObject, caller){
-// 	      // errorMessage is the raised error message
-// 	      // errorObject is either the object that caused the issue, or the nearest neighbor
-// 	      // caller is the method in which the error occurred
-// 	    } 
+	    },
+	  	error: function(errorMessage, errorObject, caller){
+	      // errorMessage is the raised error message
+	      // errorObject is either the object that caused the issue, or the nearest neighbor
+	      // caller is the method in which the error occurred
+	    } 
 		
-// 	});
-// }
+	});
+}
 
 // function sendNewImages(){
 	
