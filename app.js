5c974891556b5c3d000ad841338566718b9d2773
Julien <julien@piratalondon.com>
2012-09-07 12:27:06 +0100

    push

--- app.js
+++ app.js
@@ -70,8 +70,7 @@ app.post('/callback', function(req, res){
 	        // use POST
 	        console.log('POST.object_id', body);
 	        console.log('POST.object_id', body[0]);
-	        console.log('POST.object_id', body.object_id);
-	        console.log('POST.object_id', body[0].object_id);
+	        console.log('get(object_id)', body.get('object_id'));
 
 	 //        Instagram.tags.recent({ name: POST.object_id,
 		// 		complete: function(data, pagination){
